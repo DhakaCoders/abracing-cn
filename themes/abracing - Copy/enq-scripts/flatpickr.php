b99a78e68e29653168b761fa0896309c0f3395fa
usernamejahirrayhan <jahirrayhan6569@gmail.com>
2021-10-29 17:58:45 +0600

    ...

--- themes/abracing - Copy/enq-scripts/flatpickr.php
+++
@@ -1,3 +0,0 @@
-<?php
-wp_enqueue_style('flatpickr-css', get_template_directory_uri() . '/assets/flatpickr/flatpickr.min.css', array(), null);
-wp_enqueue_script('flatpickr-js', get_template_directory_uri() . '/assets/flatpickr/flatpickr.js', array('jquery'), '4.6.9', true);
